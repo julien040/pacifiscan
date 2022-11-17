fb23e7732a8f5645b4dc6cbebf954abe415dde84
Julien <48369040+julien040@users.noreply.github.com>
2022-11-17 19:51:24 +0100

    Unuseful text removed

--- screens/parametre.js
+++ screens/parametre.js
@@ -34,19 +34,6 @@ function Parametre({ navigation }) {
           <Spacer />
           <CommuneSelect />
           <Spacer />
-          <MediumHeading colored>Permissions de l'app :</MediumHeading>
-          <Text
-            letterSpacing={-0.3}
-            fontSize={14}
-            fontFamily="Inter_400Regular"
-            marginTop={2}
-          >
-            Nous ne pouvons malheureusement pas révoquer les permissions de
-            l'application. Si vous n'êtes plus consentant, nous vous invitons à
-            la désinstaller et contacter le support pour une suppression de
-            données
-          </Text>
-          <Spacer />
           <MediumHeading colored>Donnéees collectées</MediumHeading>
           <Text
             letterSpacing={-0.3}
@@ -58,8 +45,9 @@ function Parametre({ navigation }) {
             Cependant, nous utilisons plusieurs sous processeurs pour traiter
             ces données : Amazon Web services, Expo, OVH, Scaleway, Cloudflare,
             et Amplitude. Toutes les images des déchets scannés sont stockées
-            sur nos serveurs. Si vous souhaitez supprimer ces données, envoyez
-            un email à contact@pacifiscan.org
+            sur nos serveurs. Si vous souhaitez supprimer ces données ou vous
+            n'êtes plus consentant au partage de ces données, envoyez un email à
+            contact@pacifiscan.org
           </Text>
         </Flex>
         <Flex justify="space-between" direction="row">
